0384f2d6d189f332e0745a01af8a6c520f543417
Ubuntu <ninadk@ninadk-devserver.jkm32sxmvooezfyr4a1y51xqeh.ax.internal.cloudapp.net>
2019-02-14 20:40:06 +0000

    Tidies up Sender

--- Python/eventMessageSender.py
+++ Python/eventMessageSender.py
@@ -41,7 +41,24 @@ def _getProcessExperimentPayload(   MessageId,
     return r, str(MessageId)
 
 
-async def sendMessageAsync(client, payload, partition="0"):
+detectorsTypes = {  common._MESSAGE_CONSUMER_GRP_GOOGLE:common._MESSAGE_TYPE_DETECTOR_GOOGLE, 
+                    common._MESSAGE_CONSUMER_GRP_AZURE:common._MESSAGE_TYPE_DETECTOR_AZURE, 
+                    common._MESSAGE_CONSUMER_GRP_YOLO:common._MESSAGE_TYPE_DETECTOR_YOLO, 
+                    common._MESSAGE_CONSUMER_GRP_MOBILENET:common._MESSAGE_TYPE_DETECTOR_MOBILE_NET, 
+                    common._MESSAGE_CONSUMER_GRP_TENSORFLOW:common._MESSAGE_TYPE_DETECTOR_TENSORFLOW}
+
+def _getDetectorTypeMessagePayload(MessageId, experimentName, destinationFolder, detector ):
+    r = json.dumps({common._MESSAGE_TYPE_TAG: detector, 
+                    common._MESSAGE_TYPE_DETECTOR_MESSAGE_ID : MessageId,
+                    common._MESSAGE_TYPE_DETECTOR_EXPERIMENT_NAME : experimentName, 
+                    common._MESSAGE_TYPE_DETECTOR_CREATION_DATE_TIME : datetime.datetime.now().strftime("%c"),
+                    common._MESSAGE_TYPE_DETECTOR_DEST_FOLDER : destinationFolder} )
+    return r, str(MessageId)    
+
+
+
+async def sendMessageAsync(payload, partition="0"):
+    client = EventHubClientAsync(ADDRESS, debug=True, username=USER, password=KEY) 
     sender = client.add_async_sender(partition=partition)
     await client.run_async()
     data = EventData(payload)
@@ -58,42 +75,15 @@ async def sendStartExperimentMessage(experimentNames):
     if not ADDRESS:
         raise ValueError("No EventHubs URL supplied.") 
     
-    client = EventHubClientAsync(ADDRESS, debug=True, username=USER, password=KEY) 
+    
     lstGuids = []
     for experimentName in experimentNames:
         r, guid = _getStartExperimentMessagePayload(experimentName)
         lstGuids.append(json.dumps({experimentName:guid}))
-        
-
         try:
-            # client = EventHubClient(ADDRESS, debug=True, username=USER, password=KEY)
-            # sender = client.add_sender(partition="0") 
-            # client.run()
-
-            #loop = asyncio.get_event_loop()
-            #client = EventHubClient(ADDRESS, debug=True, username=USER, password=KEY)
-            #tasks = [asyncio.create_task(sendMessageAsync(client, n)) for n in lstPayloads]
-            #tasks = asyncio.gather[asyncio.create_task(sendMessageAsync(client, lstPayloads[0]))]
-            #start_time = time.time()
-            await sendMessageAsync(client, r, "0") 
-            #await client.stop_async()
-            # loop.run_until_complete()
-            # loop.run_until_complete()
-
-            #end_time = time.time()
-            #run_time = end_time - start_time
-            #print("Runtime: {} seconds".format(run_time))
-            #loop.close()                                                
-
-            #sender = client.add_sender(partition=next(pool)) 
-            #sender = client.add_sender(partition=next(pool)) 
-            #client.run()
-            # sender.send(EventData(r))
-            #logger.info("Runtime: {} seconds".format(run_time))
+            await sendMessageAsync(r, "0") 
         except Exception as e:
             raise
-        # finally:
-        #     client.stop()
     return   lstGuids      
 
 
@@ -110,17 +100,13 @@ async def sendProcessExperimentMessage(   MessageId,
     global USER
     global KEY
     global pool
-    print('sendProcessExperimentMessage')
+    
     guid = None
 
     if not ADDRESS:
         raise ValueError("No EventHubs URL supplied.")                                    
 
     try:
-        #loop = asyncio.get_event_loop()
-        #client = EventHubClient(ADDRESS, debug=True, username=USER, password=KEY)
-        client = EventHubClientAsync(ADDRESS, debug=True, username=USER, password=KEY)
-
         payload, guid = _getProcessExperimentPayload( MessageId,
                                                 experimentName, 
                                                 srcImageFolder,
@@ -129,63 +115,31 @@ async def sendProcessExperimentMessage(   MessageId,
                                                 pos,
                                                 partOfFileName)
 
-        #tasks = asyncio.gather(sendMessageAsync(client, payload))
-        #start_time = time.time()
-        # loop.run_until_complete(tasks)
-        # loop.run_until_complete(client.stop_async())
-        await sendMessageAsync(client, payload, next(pool)) 
-       
-
-        #end_time = time.time()
-        #run_time = end_time - start_time
-        #print("Runtime: {} seconds".format(run_time))
-        #loop.close()                                                
-
-        #sender = client.add_sender(partition=next(pool)) 
-        #sender = client.add_sender(partition=next(pool)) 
-        #client.run()
-        # sender.send(EventData(r))
-        #logger.info("Runtime: {} seconds".format(run_time))
+        await sendMessageAsync(payload, next(pool)) 
     except Exception as e:
         raise
     finally:
-        #client.stop()
         pass
     return   guid                                  
 
-
-detectorsTypes = {  common._MESSAGE_CONSUMER_GRP_GOOGLE:common._MESSAGE_TYPE_DETECTOR_GOOGLE, 
-                    common._MESSAGE_CONSUMER_GRP_AZURE:common._MESSAGE_TYPE_DETECTOR_AZURE, 
-                    common._MESSAGE_CONSUMER_GRP_YOLO:common._MESSAGE_TYPE_DETECTOR_YOLO, 
-                    common._MESSAGE_CONSUMER_GRP_MOBILENET:common._MESSAGE_TYPE_DETECTOR_MOBILE_NET, 
-                    common._MESSAGE_CONSUMER_GRP_TENSORFLOW:common._MESSAGE_TYPE_DETECTOR_TENSORFLOW}
-
-def _getDetectorTypeMessagePayload(MessageId, experimentName, destinationFolder, detector ):
-    r = json.dumps({common._MESSAGE_TYPE_TAG: detector, 
-                    common._MESSAGE_TYPE_DETECTOR_MESSAGE_ID : MessageId,
-                    common._MESSAGE_TYPE_DETECTOR_EXPERIMENT_NAME : experimentName, 
-                    common._MESSAGE_TYPE_DETECTOR_CREATION_DATE_TIME : datetime.datetime.now().strftime("%c"),
-                    common._MESSAGE_TYPE_DETECTOR_DEST_FOLDER : destinationFolder} )
-    return r, str(MessageId)    
 
 async def sendDetectorMessages(MessageId,experimentName,destinationFolder):
     global ADDRESS
     global USER
     global KEY
     global pool
-    print('sendDetectorMessages')
     guid = None
 
     if not ADDRESS:
         raise ValueError("No EventHubs URL supplied.")                                    
 
     lstGuids = []
-    client = EventHubClientAsync(ADDRESS, debug=True, username=USER, password=KEY)
+    
     for key, value in detectorsTypes.items():
         r, guid = _getDetectorTypeMessagePayload(MessageId, experimentName, destinationFolder, value )
         lstGuids.append(json.dumps({experimentName:guid}))
         try:
-            await sendMessageAsync(client, r, "0") # we need only one instance of these running! 
+            await sendMessageAsync(r, "0") # we need only one instance of these detectors running! 
         except Exception as e:
             raise
         finally:
