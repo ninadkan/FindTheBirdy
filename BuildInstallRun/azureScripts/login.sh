30a2380a4f76a966272f0ba985bc3f14d4ec2565
Ninad Kanthi <ninad_kanthi@hotmail.com>
2019-07-25 18:27:19 +0100

    login.sh no longer needs to login

--- BuildInstallRun/azureScripts/login.sh
+++ BuildInstallRun/azureScripts/login.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 source ./parameters.sh
-LoggedIn=$(az.cmd account show --query 'id')
-if [ $LoggedIn ];
-    then 
-        echo "User already logged in"
-        echo $LoggedIn 
-        pwd
-    else
-        echo "Need to login again"
-        az.cmd login
-        if [ $? -eq 0 ];
-        then
-            echo "Login Successful"
-        else
-            echo "Login Unsuccessul"
-        fi
-fi 
-echo $WEBUSERNAME
-Date
+# LoggedIn=$(az.cmd account show --query 'id')
+# if [ $LoggedIn ];
+#     then 
+#         echo "User already logged in"
+#         echo $LoggedIn 
+#         pwd
+#     else
+#         echo "Need to login again"
+#         az.cmd login
+#         if [ $? -eq 0 ];
+#         then
+#             echo "Login Successful"
+#         else
+#             echo "Login Unsuccessul"
+#         fi
+# fi 
+# echo $WEBUSERNAME
+# Date
